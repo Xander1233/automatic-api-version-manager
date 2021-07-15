586c3928956643aed6339a8669b751121f07e5c0
Xandrrrr <david.neidhart@gmx.net>
2021-07-15 14:25:08 +0200

    Update post.ts

--- src/version/v1/post.ts
+++ src/version/v1/post.ts
@@ -15,27 +15,9 @@ export class SubscriptionRoute extends APIVersion {
 		this.defaultStart = `/api/${version}/subscriptions`;
 
 		this.instance = this.app.App;
-
-
 	}
 
 	private async getPosts(req: Request, rep: Reply) {
-
-		let requestor = req.headers.authorization ?? null;
-
-		if (!requestor) {
-			rep.status(401);
-			return { code: 401, message: 'Unauthorized' };
-		}
-
-		const databaseAuthResult = await fetch(`${this.databaseUrl}/`)
-
-		const query = (<any> req.query).query ?? '@me';
-
-
-
-
-
 
 	}
 
